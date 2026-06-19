de1de26903adb57c10b7e1e667bcd2ffdca0786c
b22pchg-arch <b22pchg@gmail.com>
2026-06-19 14:01:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trac-nghiem-excel-v45-33';
+const CACHE_NAME = 'trac-nghiem-excel-v45-34';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./xlsx.full.min.js','./default-bank.json'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
